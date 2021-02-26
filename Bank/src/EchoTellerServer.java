2a9118ccbd1594fdfe90dd37c972e4dc8e8caca0
marcusk19 <mskok@ncsu.edu>
2021-02-26 18:04:21 -0500

    tellerServerClientInterface

--- Bank/src/EchoTellerServer.java
+++ Bank/src/EchoTellerServer.java
@@ -1,5 +1,6 @@
 import java.net.InetAddress;
 import java.rmi.Naming;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -10,6 +11,22 @@ public class EchoTellerServer extends UnicastRemoteObject implements tellerServe
         LocateRegistry.createRegistry(1099); // find/start rmiregistry on this computer
         Naming.rebind("EchoTellerserver", this); // register "this" server's STUB with the rmiregistry
         System.out.println("EchoTellerServer is up at " + InetAddress.getLocalHost().getHostAddress());
-        
+
+    }
+
+    public String closeOutAccount(int accountNumber, String customerName) throws RemoteException {
+        return null;
+    }
+
+    public String createNewAccount(String accountType, String customerName) throws RemoteException {
+        return null;
+    }
+
+    public String processAccount(String processType, int accountNumber, double amount, String customerName) throws RemoteException {
+        return null;
+    }
+
+    public String showAccount(int accountNumber, String customerName) throws RemoteException {
+        return null;
     }
 }
